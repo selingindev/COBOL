e9f30f25c46b4778f7c7d90ed0ad242bbf156518
selingindev <88356338+selingindev@users.noreply.github.com>
2025-12-09 12:11:44 -0300

    first commit

---
+++ hello_wolrd/hello_world.cbl
@@ -0,0 +1,17 @@
+000001 IDENTIFICATION DIVISION.
+      *suas informações aqui
+000002     PROGRAM-ID. HELLO-WOLRD.
+000003     AUTHOR. Arthur Selingin.
+000004
+000005 ENVIRONMENT DIVISION.
+      * Definições de ambiente aqui
+000007 DATA DIVISION.    
+      * variaveis aqui
+
+000009 PROCEDURE DIVISION.
+      * Lógica do programa aqui
+       PRINCIPAL.
+           DISPLAY "HELLO, WOLRD!".
+      
+       STOP RUN.
+       END PROGRAM HELLO-WOLRD.
