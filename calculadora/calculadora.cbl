32c3bbe97b9bca2a0e48c05f03b2f1025e293063
selingindev <88356338+selingindev@users.noreply.github.com>
2025-12-10 20:14:24 -0300

    COMEÇANDO

---
+++ calculadora/calculadora.cbl
@@ -0,0 +1,12 @@
+       identification division.
+           program-id calculadora.
+
+       environment division.
+       
+       data division.
+           WORKING-STORAGE SECTION.
+           01      
+       procedure division
+
+       stop run.
+       end program calculadora.
