29b19014682fa0f2c3d2413b328d89b24425a17a
selingindev <88356338+selingindev@users.noreply.github.com>
2025-12-09 23:12:45 -0300

    primeira vez usando variaveis

--- hello_wolrd/hello_world.cbl
+++ hello_wolrd/hello_world.cbl
@@ -1,17 +1,19 @@
 000001 IDENTIFICATION DIVISION.
-      *suas informações aqui
+      *suas informações aqui.
 000002     PROGRAM-ID. HELLO-WOLRD.
 000003     AUTHOR. Arthur Selingin.
 000004
 000005 ENVIRONMENT DIVISION.
-      * Definições de ambiente aqui
+      * Definições de ambiente aqui.
 000007 DATA DIVISION.    
-      * variaveis aqui
-
+      * variaveis aqui.
+       WORKING-STORAGE SECTION.
+       77 W-MENSAGEM PIC X(31) VALUE "HELLO, WOLRD! SOU UM DEV COBOL".
 000009 PROCEDURE DIVISION.
-      * Lógica do programa aqui
+
+      * Lógica do programa aqui.
        PRINCIPAL.
-           DISPLAY "HELLO, WOLRD!".
+           DISPLAY W-MENSAGEM.
       
        STOP RUN.
        END PROGRAM HELLO-WOLRD.
