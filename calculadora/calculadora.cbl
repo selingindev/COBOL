3fa9e287215beca91cd4aa58ea1d6a481b94d44d
selingindev <88356338+selingindev@users.noreply.github.com>
2025-12-11 08:45:22 -0300

    criando variaveis e sucesso no switch case

--- calculadora/calculadora.cbl
+++ calculadora/calculadora.cbl
@@ -5,22 +5,54 @@
        
        data division.
        WORKING-STORAGE SECTION.
-       01 W-NUMERO01  PIC 9(009)V9(002).
-       01 W-NUMERO02  PIC 9(009)V9(002).
-       01 W-RESULTADO PIC 9(018)V9(002).    
+       01 W-NUMERO01  PIC 9(003)V9(002).
+       01 W-NUMERO02  PIC 9(003)V9(002).
+       01 W-RESULTADO PIC 9(004)V9(002). 
+       01 W-OPERRATION.
+           05 W-SOMA PIC 9(001) VALUE 01.   
+           05 W-SUBTRACAO PIC 9(001) VALUE 02.   
+           05 W-MULTIPLICACAO PIC 9(001) VALUE 03.   
+           05 W-DIVISAO PIC 9(001) VALUE 04.   
+
        procedure division.
        PRINCIPAL.
-
            display "DIGITE O PRIMEIRO NUMERO: ".
            accept W-NUMERO01
 
            display "DIGITE O SEGUNDO NUMERO: ".
            accept W-NUMERO02
 
-           add W-NUMERO01 TO W-NUMERO02 GIVING W-RESULTADO. 
+           display "===============MENU================" &
+            "1 - SOMA" &
+            "2 - SUBTRACAO" &
+            "3 - MULTIPLICACAO" &
+            "4 - DIVISAO" &
+            "===================================".
+            accept W-OPERRATION
 
+           evaluate W-OPERRATION
+               when W-SOMA
+                   perform SOMA
+               when W-SUBTRACAO
+                   perform SUBTRACAO
+               when W-MULTIPLICACAO
+                   perform MULTIPLICACAO
+               when W-DIVISAO
+                   perform DIVISAO
+               when other
+                   display "OPERACAO INVALIDA!".    
+                   perform PRINCIPAL
+     
+
+           exit.
+      
+       SOMA.
+           add W-NUMERO01 TO W-NUMERO02 GIVING W-RESULTADO. 
            display "O RESULTADO DA SOMA E: " W-RESULTADO.
 
+           exit.
+
+       SUBTRACAO.
+           exit  .    
        stop run.
        end program CALCULADORA.
-       
