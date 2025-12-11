2d4918758b34a26ef3abce768fe73b55e8eb9659
selingindev <88356338+selingindev@users.noreply.github.com>
2025-12-11 11:53:21 -0300

    montando loop para conseguir realizar varias operações sem reiniciar o programa

--- calculadora/calculadora.cbl
+++ calculadora/calculadora.cbl
@@ -7,52 +7,89 @@
        WORKING-STORAGE SECTION.
        01 W-NUMERO01  PIC 9(003)V9(002).
        01 W-NUMERO02  PIC 9(003)V9(002).
-       01 W-RESULTADO PIC 9(004)V9(002). 
-       01 W-OPERRATION.
-           05 W-SOMA PIC 9(001) VALUE 01.   
-           05 W-SUBTRACAO PIC 9(001) VALUE 02.   
-           05 W-MULTIPLICACAO PIC 9(001) VALUE 03.   
-           05 W-DIVISAO PIC 9(001) VALUE 04.   
+       01 W-RESULTADO PIC S9(004)V9(002). 
+
+      *Formatacao de numeros
+       01 W-RESULTADO-FORM PIC -ZZZ.99 .
+
+      * Const para as operacões básicas, 
+       01 W-OPERRATION PIC 9(1).
+       01 CONST-SOMA          PIC 9(1) VALUE 1.
+       01 CONST-SUBTRACAO     PIC 9(1) VALUE 2.
+       01 CONST-MULTIPLICACAO PIC 9(1) VALUE 3.
+       01 CONST-DIVISAO       PIC 9(1) VALUE 4.
+
+      *boolean para controle de fluxo
+       01 W-BOOLEAN PIC X(1).
+           88 CONTINUAR VALUE "T".
+           88 PARAR VALUE "F".
+
 
        procedure division.
        PRINCIPAL.
            display "DIGITE O PRIMEIRO NUMERO: ".
-           accept W-NUMERO01
+           accept W-NUMERO01.
 
            display "DIGITE O SEGUNDO NUMERO: ".
-           accept W-NUMERO02
+           accept W-NUMERO02.
 
-           display "===============MENU================" &
-            "1 - SOMA" &
-            "2 - SUBTRACAO" &
-            "3 - MULTIPLICACAO" &
-            "4 - DIVISAO" &
-            "===================================".
-            accept W-OPERRATION
+           display "===============MENU================".
+           display "1 - SOMA".  
+           display "2 - SUBTRACAO".
+           display "3 - MULTIPLICACAO".
+           display "4 - DIVISAO".
+           display  "===================================".
+           accept W-OPERRATION.
 
            evaluate W-OPERRATION
-               when W-SOMA
+               when CONST-SOMA
                    perform SOMA
-               when W-SUBTRACAO
+               when CONST-SUBTRACAO
                    perform SUBTRACAO
-               when W-MULTIPLICACAO
-                   perform MULTIPLICACAO
-               when W-DIVISAO
+               when CONST-MULTIPLICACAO
+                   perform MULTIPLICACAO 
+               when CONST-DIVISAO
                    perform DIVISAO
                when other
-                   display "OPERACAO INVALIDA!".    
-                   perform PRINCIPAL
-     
-
+                   display "OPERACAO INVALIDA!"
+           end-evaluate.
+           
+           perform NEWCOUNT
            exit.
-      
+          
+       NEWCOUNT.
+           display "DESEJA REALIZAR OUTRA OPERACAO? (T/F)".
+           accept W-BOOLEAN.
+
+           IF CONTINUAR
+               perform PRINCIPAL
+           ELSE
+               display "OBRIGADO POR USAR O PROGRAMA".
+           
+           exit.
        SOMA.
            add W-NUMERO01 TO W-NUMERO02 GIVING W-RESULTADO. 
-           display "O RESULTADO DA SOMA E: " W-RESULTADO.
-
+           move W-RESULTADO TO W-RESULTADO-FORM.
+           display "O resultado da soma e: " W-RESULTADO-FORM.
            exit.
 
        SUBTRACAO.
-           exit  .    
+           subtract W-NUMERO02 from W-NUMERO01 GIVING W-RESULTADO.
+           move W-RESULTADO TO W-RESULTADO-FORM.
+           display "O resultado da subtracao e: " W-RESULTADO-FORM.
+           exit.
+
+       MULTIPLICACAO. 
+           multiply W-NUMERO01 BY W-NUMERO02 GIVING W-RESULTADO.
+           move W-RESULTADO TO W-RESULTADO-FORM.
+           display "O resultado da multiplicacao e: " W-RESULTADO-FORM.
+           exit.
+
+       DIVISAO.
+           divide W-NUMERO01 BY W-NUMERO02 GIVING W-RESULTADO.
+           move W-RESULTADO TO W-RESULTADO-FORM.
+           display "O resultado da divisao e: " W-RESULTADO-FORM.
+           exit. 
+       
        stop run.
        end program CALCULADORA.
